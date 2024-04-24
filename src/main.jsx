2bc3b89501640eb0bb9e25a1859c3ab350b5439a
Bayaman <toktobekovbayaman@gmail.com>
2024-04-24 12:32:58 +0600

    Изменение # на пустую строку

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import { HashRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
    <React.StrictMode>
-      <HashRouter>
+     <HashRouter basename={process.env.PUBLIC_URL}>
          <App />
       </HashRouter>
    </React.StrictMode>
